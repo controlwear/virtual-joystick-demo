e22255364ddfd9c448e860f1abb3df432d2bd4a2
Damien Brun <makowildcat@gmail.com>
2017-06-05 23:06:25 -0400

    Show updated value of the joysticks (strength and angle)

--- app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
+++ app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -1,13 +1,49 @@
 package io.github.controlwear.joystickdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mTextViewAngleLeft;
+    private TextView mTextViewStrengthLeft;
+
+    private TextView mTextViewAngleRight;
+    private TextView mTextViewStrengthRight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewAngleLeft = (TextView) findViewById(R.id.textView_angle_left);
+        mTextViewStrengthLeft = (TextView) findViewById(R.id.textView_strength_left);
+
+        JoystickView joystickLeft = (JoystickView) findViewById(R.id.joystickView_left);
+        joystickLeft.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleLeft.setText(angle + "°");
+                mTextViewStrengthLeft.setText(strength + "%");
+            }
+        });
+
+
+        mTextViewAngleRight = (TextView) findViewById(R.id.textView_angle_right);
+        mTextViewStrengthRight = (TextView) findViewById(R.id.textView_strength_right);
+
+        JoystickView joystickRight = (JoystickView) findViewById(R.id.joystickView_right);
+        joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleRight.setText(angle + "°");
+                mTextViewStrengthRight.setText(strength + "%");
+            }
+        });
     }
 }
