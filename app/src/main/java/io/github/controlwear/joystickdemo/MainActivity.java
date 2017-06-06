2c85b62702d914e6e3deda4a4b8e871d824e4417
Damien Brun <makowildcat@gmail.com>
2017-06-05 22:02:21 -0400

    Init: create app

---
+++ app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.controlwear.joystickdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
